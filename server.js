512069f37b9ebd25ba36ee24455a3d90194aea2c
RahulB2511 <57500799+RahulB2511@users.noreply.github.com>
2020-04-25 17:29:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,35 +11,39 @@ const app = express();
 const bodyParser = require('body-parser')
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
 
 /* Initialize the main project folder*/
 app.use(express.static('website'));
-//Decided to use an uncommon port for expiremental purposes
+
 const port = 8000;
 /* Spin up the server*/
 const server = app.listen(port, listening);
  function listening(){
-    // console.log(server);
     console.log(`running on localhost: ${port}`);
   };
 
+
 // GET route
-app.get('/all', sendData);
+app.get('/get', sendData);
 
 function sendData (request, response) {
+  console.log("sendData called")
   response.send(projectData);
 };
 
-// POST route
-//Combine the two POSTS Functions into one
-app.post('/add', callBack);
-function callBack (req,res){
-  Object.assign(projectData,req.body);
-  res.send(true);
-};
+
+app.post('/addForecast', addForecast);
+
+function addForecast (request, response){
+    const body = request.body;
+    projectData.temperature = body.temperature;
+    projectData.date = body.date;
+    projectData.user_response = body.user_response;
+    console.log(projectData);
+    const jsonData = JSON.parse('{"response": "POST received"}');
+    response.send(jsonData);
+}
