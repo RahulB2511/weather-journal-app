49208b28c7210ce03b37e2401c74361c536b8838
RahulB2511 <57500799+RahulB2511@users.noreply.github.com>
2020-04-25 17:26:58 +0530

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,107 +1,70 @@
-/* Global Variables */
-// Personal API Key for OpenWeatherMap API
-//w3Sschool and Stack Overflow helped with the code and underestanding
-const apiKey = '48c20a7828f892dc0b943b5f17a33a06';
-const apiURL = 'http://api.openweathermap.org/data/2.5/weather?q=London,gb&appid=6d101e81f2140d2f516e6072bda48dec&units=imperial'
-// Create a new date instance dynamically with JS
+/*Global Variables*/
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = '&APPID=48c20a7828f892dc0b943b5f17a33a06';
+const countryCode = ',in';
+const fahrenheit = '&units=imperial';
+
+//Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+d.getDate()+'.'+d.getFullYear();
 
 
-//Help From "Updating UI Elements"
-// Event listener to add function to existing HTML DOM element
 document.getElementById('generate').addEventListener('click', performAction);
-/* Function called by event listener */
-function performAction(e) {
-    const feelText = document.getElementById('feelings').value;
 
-    // Friends helped with getting API data
-    getApiData(apiURL, document.getElementById('zip').value, apiKey)
-    .then(function (APItemperature) {
 
-        postData('/add', { temperature: APItemperature, date: newDate, userResponse: feelText });
-
-    })
-    .then(function (){
-        updateUI();
-        })
+function performAction(e){
+    const cityZipCode = document.getElementById('zip').value;
+    getData(baseURL+cityZipCode+countryCode+apiKey+fahrenheit)
+    .then(
+        function(weather) {
+            const feelings = document.getElementById('feelings').value;
+            return postData('/addForecast', {temperature: weather.main.temp, date: newDate, user_response: feelings})
+        }
+    )
+    .then(
+        function(post_response) {
+            return getData('/get')
+        }
+    )
+    .then(
+        function(get_response) {
+            document.getElementById('date').innerHTML = get_response.date;
+            document.getElementById('temp').innerHTML = get_response.temperature;
+            document.getElementById('content').innerHTML = get_response.user_response;
+        }
+    )
 }
 
 
-/* Function to GET Web API Data*/
-const getApiData = async (apiURL, zip, apiKey) => {
-    const response = await fetch(apiURL + zip + ',us&appid=' + apiKey);
-    try {
-        const webData = await response.json();
-        APItemperature = webData.main.temp;
-        return APItemperature
-    } catch (error) {
-        console.log("error", error);
-    }
-}
-
-
-
-
-
-
-
-
-/* Function to GET Project Data */
-//Help From "Async GET"
-const retrieveData = async (url='') =>{ 
-    const request = await fetch(url);
-    try {
-    // Transform into JSON
-    const allData = await request.json()
-    }
-    catch(error) {
-      console.log("error", error);
-      // appropriately handle the error
-    }
-  };
-
-
-
-
-
-
-//Help from "Async GET"
-/* Function to POST data */
-const postData = async ( url = '', data = {})=>{
-
+const postData = async (url = '', data = {})=>{
+    // console.log(data);
     const response = await fetch(url, {
-    method: 'POST', 
-    credentials: 'same-origin', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header        
-  });
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
 
     try {
-      const newData = await response.json();
-      return newData;
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
     }catch(error) {
-    console.log("error", error);
+        console.log("error", error);
     }
 };
 
 
-//Update the index page
-//Help From "Updating UI Elements"
-//THis is all the way in the bottom
-const updateUI = async () => {
-    const request = await fetch('/all')
+const getData = async (url = '')=>{
+    const response = await fetch(url);
 
-    try{
-        const recRec = await request.json();
-        console.log(recRec);
-        document.getElementById('date').innerHTML = 'Date: ' + recRec.date;
-        document.getElementById('temp').innerHTML = 'Temperature: ' + Math.round((recRec.temperature*1.8) - 459.7) + '&#8457;';
-        document.getElementById('content').innerHTML = 'Thoughts: ' + recRec.userResponse;
-    
-    }catch(error){
-        console.log("error",error)
+    try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+    }catch(error) {
+        console.log("error", error);
     }
-}
+};
